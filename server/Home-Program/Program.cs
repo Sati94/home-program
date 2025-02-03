c10e54728452a9c63b00fdf914ae3a5424eef70c
Ati <nagyonkellvalamihez@gmail.com>
2025-02-03 11:55:23 +0100

    The auth maked, refresh the databes (new migration because ApplicationUser andf IdentityUser), Login,Register are working

--- server/Home-Program/Program.cs
+++ server/Home-Program/Program.cs
@@ -2,6 +2,12 @@
 using Microsoft.EntityFrameworkCore;
 using Home_Program.Data;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Home_Program.Model.User;
+using Home_Program.Services.Authentication;
 
 namespace Home_Program
 {
@@ -21,6 +27,30 @@ namespace Home_Program
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                options.UseNpgsql(connectionString));
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+            });
+            
             
             builder.Services.AddControllers();
            
@@ -38,6 +68,7 @@ namespace Home_Program
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
---
+++ server/Home-Program/Services/Authentication/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Home_Program.Model.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Home_Program.Services.Authentication
+{
+    public interface IAuthService
+    {
+        Task<IActionResult> Login(LoginModel model);
+        Task<IActionResult> Register(RegisterModel model);
+    }
+}
